e1405cdf92c8cefb624a4c514ab105690898b198
Joe Tustin <J.Tustin@gmail.com>
2025-07-07 18:48:29 -0600

    adding in new analytics/

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Game from './components/Game';
 import './styles/game.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
+      <div className="tiktok-link">
+        <a href="https://www.tiktok.com/@vibinwiththechef" target="_blank" rel="noopener noreferrer">
+          <img src="/Images/logo.png" alt="VibinWithTheChef TikTok" />
+        </a>
+      </div>
       <Game />
+      <Analytics />
     </div>
   );
 }
